08240af56f3dec7b72c36629bd49f83a49675656
Max Brämer <max.braemer@tu-dortmund.de>
2020-04-28 15:25:54 +0200

    'update'

--- server.js
+++ server.js
@@ -71,7 +71,10 @@ app.put('/api/games/:id', function (req, res) {
   var updateDoc = req.body;
   delete updateDoc._id;
 
-  db.collection(GAMES_COLLECTION).updateOne({ _id: new ObjectID(req.params.id) }, updateDoc, function (err, doc) {
+  db.collection(GAMES_COLLECTION).findOneAndUpdate({ _id: new ObjectID(req.params.id) }, updateDoc, function (
+    err,
+    doc
+  ) {
     if (err) {
       handleError(res, err.message, 'Failed to update game');
     } else {
